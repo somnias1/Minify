9932694152aea4be050f315d5ae39964de8375a2
Lizardo <lizardo@utp.edu.co>
2022-01-13 13:14:25 -0500

    Artist serializer + Viewset

---
+++ API/serializers/artists.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .. models import Artist
+
+class ArtistCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ("artist_name", "language", "description")
+
+class ArtistInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ("id","artist_name","language","description")
+
---
+++ API/views/artists.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from ..models import Artist
+
+from ..serializers import ArtistInfoSerializer
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistInfoSerializer
